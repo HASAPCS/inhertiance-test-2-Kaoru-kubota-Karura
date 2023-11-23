2b8e26d68f78a344d6adcc529190053855be7f42
Kaoru-kubota-Karura <cosmo.kaoru1211@gmail.com>
2023-11-23 10:11:57 +0800

    ver.α

--- src/main/java/com/example/Appliance.java
+++ src/main/java/com/example/Appliance.java
@@ -1,40 +1,49 @@
 package com.example;
 
 public class Appliance {
-    
-
-    // Constructor to initialize an ElectronicDevice object
-    public Appliance(String brand, String model) {
-        // TODO: Initialize the common attributes (brand, model) with the provided parameters
-    }
-
-    // TODO: Implement getters (and setters if necessary) for the brand and model.
-
-    // OPTIONALLY implement the required methods to pass the BonusTest.
-
-}
-
-package com.example;
-
-public class Appliance {
     // Attributes
     // TODO: Declare common attributes for electronic devices, such as brand and model and powerUsage.
-
+        private String brand;
+        private String model;
+        private int powerUsage;
     // Constructor
     public Appliance(String brand, String model, int powerUsage) {
         // TODO: Initialize all attributes
+        this.brand = brand;
+        this.model = model;
+        this.powerUsage = powerUsage;
     }
 
+    public String getBrand(){
+        return this. brand;
+    }
+    public String getModel(){
+        return this. model;
+    }
+    public int getPowerUsage(){
+        return this. powerUsage;
+    }
     // Getters
     // TODO: Implement getters for brand, model, and powerUsage
 
+    // public String displayInfo(){
+    //     return getBrand()+getModel()+getPowerUsage();
+    // } 
+    public String displayInfo() {
+        return "Brand: " + this.brand + ", Model: " + this.model + ", Power Usage: " + this.powerUsage + " Watts";
+    }
     // displayInfo Method
     // TODO: Implement a method that returns information about the appliance, including brand, model, and power usage.
     // Check the tests for more details
 
+    public boolean isEcoFriendly() {
+        // Check if the appliance is eco-friendly based on power usage
+        return this.powerUsage < 400;
+    }
     // isEcoFriendly Method
     // TODO: Implement a method to check if the appliance is eco-friendly based on its power usage. 
     // An appliance that uses less than 400 Watts of power is eco-friendly.
 
 }
+
 
